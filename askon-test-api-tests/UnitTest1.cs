dd00ee6671783db614c7365598346b4b75ceb052
Глеб Безруков <glebon84@gmail.com>
2023-02-07 11:22:42 +0300

    структура проектов

---
+++ askon-test-api-tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace askon_test_api_tests;
+
+public class UnitTest1
+{
+	[Fact]
+	public void Test1()
+	{
+	}
+}
---
+++ askon-test-dal-integration-tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace askon_test_dal_integration_tests;
+
+public class UnitTest1
+{
+	[Fact]
+	public void Test1()
+	{
+	}
+}
