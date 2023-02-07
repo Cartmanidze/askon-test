9119a7cc4cfde58e072d02f6e33a8d3995c6bc0e
Глеб Безруков <glebon84@gmail.com>
2023-02-07 12:42:52 +0300

    AddDalServices

--- askon-test-api/Program.cs
+++ askon-test-api/Program.cs
@@ -1,8 +1,12 @@
+using askon_test_dal;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDalServices(builder.Configuration);
 
 var app = builder.Build();
 
