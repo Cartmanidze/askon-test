e71f724bb0f135ca755b23459322a722b1da55bf
Aleksey Fedoseev <aleksey.fedoseev@sphaera.ru>
2023-02-08 12:19:14 +0300

    Удалить

---
+++ askon-test-application/contracts/UserDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace askon_test_application.contracts
+{
+    internal class UserDto
+    {
+    }
+}
