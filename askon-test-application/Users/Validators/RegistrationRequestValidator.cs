3b5a8c01fe45895189b058be27a889a737031209
Глеб Безруков <g.bezrukov@reo.ru>
2023-02-08 19:15:53 +0600

    Добавил RegistrationRequestValidator

---
+++ askon-test-application/Users/Validators/RegistrationRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using askon_test_application.Users.Requests;
+using FluentValidation;
+
+namespace askon_test_application.Users.Validators;
+
+/// <inheritdoc />
+public sealed class RegistrationRequestValidator : AbstractValidator<RegistrationRequest>
+{
+	/// <inheritdoc />
+	public RegistrationRequestValidator() => RuleFor(x => x.Password)
+		.NotEmpty();
+}
