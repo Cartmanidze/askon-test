aeb90327457e45ea195a5a3ebffe7fbab9a46edf
Глеб Безруков <glebon84@gmail.com>
2023-02-08 14:57:54 +0300

    Рефактоирнг

---
+++ askon-test-api/Views/RegistrationView.cs
@@ -0,0 +1,22 @@
+﻿namespace askon_test_api.Views;
+
+/// <summary>
+/// Представление для регистрации
+/// </summary>
+public class RegistrationView
+{
+	/// <summary>
+	/// Логин
+	/// </summary>
+	public string Login { get; set; } = null!;
+
+	/// <summary>
+	/// Ник
+	/// </summary>
+	public string NickName { get; set; } = null!;
+
+	/// <summary>
+	/// Пароль
+	/// </summary>
+	public string Password { get; set; } = null!;
+}
