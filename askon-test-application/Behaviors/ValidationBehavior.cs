a54701c5fd27f678f3832d365701f42f16f7210d
Глеб Безруков <glebon84@gmail.com>
2023-02-08 14:59:51 +0300

    Рефакторинг

--- askon-test-application/Behaviors/ValidationBehavior.cs
+++ askon-test-application/Behaviors/ValidationBehavior.cs
@@ -1,29 +1,24 @@
-using MediatR;
 using FluentValidation;
-using System.Threading;
-using System.Threading.Tasks;
+using MediatR;
 
-namespace askon_test_application.Behaviors
+namespace askon_test_application.Behaviors;
+
+/// <inheritdoc />
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+	where TRequest : IRequest<TResponse>
 {
+	private readonly IValidator<TRequest> _validator;
+
 	/// <summary>
-	/// 
+	/// .ctor
 	/// </summary>
-	/// <typeparam name="TRequest"></typeparam>
-	/// <typeparam name="TResponse"></typeparam>
-	public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-		where TRequest : MediatR.IRequest<TResponse>
+	public ValidationBehavior(IValidator<TRequest> validator) => _validator = validator;
+
+	/// <inheritdoc />
+	public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
 	{
-		private readonly IValidator<TRequest> _validator;
+		_validator.ValidateAndThrow(request);
 
-		public ValidationBehavior(IValidator<TRequest> validator)
-		{
-			_validator = validator;
-		}
-
-		public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
-		{
-			_validator.ValidateAndThrow(request);
-			return next();
-		}
+		return next();
 	}
 }
--- askon-test-application/Users/Validators/LoginRequestValidator.cs
+++ askon-test-application/Users/Validators/LoginRequestValidator.cs
@@ -1,22 +1,14 @@
 using askon_test_application.Users.Requests;
 using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace askon_test_application.Users.Validators
+namespace askon_test_application.Users.Validators;
+
+/// <summary>
+/// Валидатор для запроса на логин
+/// </summary>
+public sealed class LoginRequestValidator : AbstractValidator<LoginRequest>
 {
-	/// <summary>
-	/// Валидатор для запроса на логин
-	/// </summary>
-	public sealed class LoginRequestValidator : AbstractValidator<LoginRequest>
-	{
-		/// <inheritdoc />
-		public LoginRequestValidator()
-		{
-			RuleFor(x => x.Password).NotEmpty();
-		}
-	}
+	/// <inheritdoc />
+	public LoginRequestValidator() => RuleFor(x => x.Password)
+		.NotEmpty();
 }
