ba30fbab4ba4664e3d78642f381f3936e7a6879e
Глеб Безруков <glebon84@gmail.com>
2023-02-10 10:19:54 +0300

    merge

---
+++ askon-test-api/Views/LoginView.cs
@@ -0,0 +1,17 @@
+﻿namespace askon_test_api.Views;
+
+/// <summary>
+/// Представление для логина
+/// </summary>
+public class LoginView
+{
+	/// <summary>
+	/// Эл.почта
+	/// </summary>
+	public string Email { get; set; } = null!;
+
+	/// <summary>
+	/// Пароль
+	/// </summary>
+	public string Password { get; set; } = null!;
+}
