a9339a4b1e86692c2fc8ba22391b3278b1128202
Глеб Безруков <glebon84@gmail.com>
2023-02-10 11:02:21 +0300

    Рефакторинг

--- askon-test-api/Middleware/ExceptionHandlingMiddleware.cs
+++ askon-test-api/Middleware/ExceptionHandlingMiddleware.cs
@@ -1,72 +1,77 @@
+using System.Text.Json;
 using askon_test_application.Exception;
 using askon_test_domain.Exceptions;
-using System.Text.Json;
+using ApplicationException = askon_test_domain.Exceptions.ApplicationException;
 
-namespace askon_test_api.Middleware
+namespace askon_test_api.Middleware;
+
+internal sealed class ExceptionHandlingMiddleware : IMiddleware
 {
-	internal sealed class ExceptionHandlingMiddleware : IMiddleware
+	public async Task InvokeAsync(HttpContext context, RequestDelegate next)
 	{
-		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		try
 		{
-			try
+			await next(context);
+		}
+		catch (Exception e)
+		{
+			await HandleExceptionAsync(context, e);
+		}
+	}
+
+	private static async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+	{
+		var statusCode = GetStatusCode(exception);
+
+		var response = new
+		{
+			title = GetTitle(exception),
+			status = statusCode,
+			detail = exception.Message,
+			errors = GetErrors(exception)
+		};
+
+		httpContext.Response.ContentType = "application/json";
+
+		httpContext.Response.StatusCode = statusCode;
+
+		await httpContext.Response.WriteAsync(JsonSerializer.Serialize(response));
+	}
+
+	private static int GetStatusCode(Exception exception) => exception switch
+	{
+		BadRequestException => StatusCodes.Status400BadRequest,
+		NotFoundException => StatusCodes.Status404NotFound,
+		ValidationException => StatusCodes.Status422UnprocessableEntity,
+		var _ => StatusCodes.Status500InternalServerError
+	};
+
+	private static string GetTitle(Exception exception) => exception switch
+	{
+		ApplicationException applicationException => applicationException.Title,
+		_ => "Server Error"
+	};
+
+	private static IReadOnlyDictionary<string, string[]>? GetErrors(Exception exception)
+	{
+		IReadOnlyDictionary<string, string[]>? errors = null;
+
+		if (exception is ValidationException validationException)
+		{
+			errors = validationException.ErrorsDictionary;
+		} else
+		{
+			errors = new Dictionary<string, string[]>
 			{
-				await next(context);
-			}
-			catch (Exception e)
-			{
-				await HandleExceptionAsync(context, e);
-			}
+				{
+					"Exception", new[]
+					{
+						exception.Message
+					}
+				}
+			};
 		}
 
-		private static async Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
-		{
-			var statusCode = GetStatusCode(exception);
-
-			var response = new
-			{
-				title = GetTitle(exception),
-				status = statusCode,
-				detail = exception.Message,
-				errors = GetErrors(exception)
-			};
-
-			httpContext.Response.ContentType = "application/json";
-
-			httpContext.Response.StatusCode = statusCode;
-
-			await httpContext.Response.WriteAsync(JsonSerializer.Serialize(response));
-		}
-
-		private static int GetStatusCode(Exception exception) =>
-			exception switch
-			{
-				BadRequestException => StatusCodes.Status400BadRequest,
-				NotFoundException => StatusCodes.Status404NotFound,
-				ValidationException => StatusCodes.Status422UnprocessableEntity,
-				_ => StatusCodes.Status500InternalServerError
-			};
-
-		private static string GetTitle(Exception exception) =>
-			exception switch
-			{
-				askon_test_domain.Exceptions.ApplicationException applicationException => applicationException.Title,
-				_ => "Server Error"
-			};
-
-		private static IReadOnlyDictionary<string, string[]> GetErrors(Exception exception)
-		{
-			IReadOnlyDictionary<string, string[]> errors = null;
-
-			if (exception is ValidationException validationException)
-			{
-				errors = validationException.ErrorsDictionary;
-			}
-			else
-			{
-				errors = new Dictionary<string, string[]>() { { "Exception", new string[] { exception.Message } } };
-			}
-
-			return errors;
-		}
+		return errors;
 	}
 }
--- askon-test-application/Exception/ValidationException.cs
+++ askon-test-application/Exception/ValidationException.cs
@@ -6,11 +6,11 @@ namespace askon_test_application.Exception;
 public sealed class ValidationException : ApplicationException
 {
 	/// <inheritdoc />
-	public ValidationException(IReadOnlyDictionary<string, string[]> errorsDictionary)
+	public ValidationException(IReadOnlyDictionary<string, string[]>? errorsDictionary)
 		: base("Validation Failure", "One or more validation errors occurred") => ErrorsDictionary = errorsDictionary;
 
 	/// <summary>
 	/// Ошибки
 	/// </summary>
-	public IReadOnlyDictionary<string, string[]> ErrorsDictionary { get; }
+	public IReadOnlyDictionary<string, string[]>? ErrorsDictionary { get; }
 }
